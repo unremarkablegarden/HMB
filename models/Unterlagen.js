0daa557b081de6acd1afd28d71d7cde507b63243
Olle Holmberg <olle@unremarkablegarden.com>
2017-02-09 13:25:48 +0100

    downloads in unterlagen

--- models/Unterlagen.js
+++ models/Unterlagen.js
@@ -15,12 +15,13 @@ var Types = keystone.Field.Types;
 
  Unterlagen.add({
  	title: { type: String, required: true, index: true, initial: true },
-  number: { type: Types.Number, label: 'Antrag Nr.' },
- 	type: { type: Types.Select, options: 'resolutionen, referenzen', default: 'resolutionen', initial: true, index: true },
-  textNumbered: { type: Types.Code, language: 'html', label: 'Text with numbered lines' },
+  number: { type: Types.Number, label: 'Antrag Nr.', dependsOn: { type: 'resolutionen' } },
+ 	type: { type: Types.Select, options: 'resolutionen, referenzen, downloads', default: 'resolutionen', initial: true, index: true },
+  textNumbered: { type: Types.Code, language: 'html', label: 'Text with numbered lines', dependsOn: { type: 'resolutionen' } },
   text: { type: Types.Html, wysiwyg: true, label: 'Info' },
-  date: { type: Types.Date, default: Date.now },
-  pdf: { type: Types.Url, label: 'Google Docs embed URL' }
+  date: { type: Types.Date, default: Date.now, dependsOn: { type: 'resolutionen' } },
+  pdf: { type: Types.Url, label: 'Google Docs embed URL' },
+  download: { type: Types.Url, label: '(or) Download URL', dependsOn: { type: 'downloads' } },
 
  });
 
